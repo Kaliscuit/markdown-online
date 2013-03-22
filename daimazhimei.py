e50cfbdbdf1795fdfbe0adf47f0933e1a1347664
xiaozhui <yuhanjian@yahoo.com>
2013-03-22 15:55:16 +0800

    pagination

--- daimazhimei.py
+++ daimazhimei.py
@@ -4,6 +4,7 @@ import sqlite3
 import codecs
 import markdown
 from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash, send_from_directory
+from flask.ext.paginate import Pagination
 
 
 # configuration
@@ -13,6 +14,7 @@ SECRET_KEY = 'SunnyKale Daimazhimei'
 USERNAME = 'demo'
 PASSWORD = 'demo'
 MD_FOLDER = 'md'
+PAGE_COUNT = 2
 
 # app
 app = Flask(__name__)
@@ -22,25 +24,25 @@ app.config.from_envvar('DAIMAZHIMEI_SETTINGS', silent=True)
 
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
-    
-    
+
+
 def init_db():
     with closing(connect_db()) as db:
         with app.open_resource('db/schema.sql') as f:
             db.cursor().executescript(f.read())
         db.commit()
-        
-        
+
+
 @app.before_request
 def before_request():
     g.db = connect_db()
-    
-    
+
+
 @app.teardown_request
 def teardown_request(exception):
     g.db.close()
-    
-    
+
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     error = None
@@ -54,22 +56,29 @@ def login():
             flash('You were logged in')
             return redirect(url_for('index'))
     return render_template('login.html', error=error)
-        
-        
+
+
 @app.route('/logout')
 def logout():
     session.pop('logged_in', None)
     flash('You were logged out')
     return redirect(url_for('index'))
-    
-    
+
+
 @app.route('/')
-def index():
-    cur = g.db.execute('select title, time, slug from article order by id desc')
+@app.route('/<int:page_number>')
+def index(page_number=1):
+    all = g.db.execute('select count(*) from article')
+    all = all.fetchall()
+    count = all[0]
+    print count
+    cur = g.db.execute('select title, time, slug from article order by id desc limit ? offset ? ',
+                       [PAGE_COUNT, PAGE_COUNT * (page_number - 1)])
     article = [dict(title=row[0], time=row[1], slug=row[2]) for row in cur.fetchall()]
-    return render_template('index.html', articles=article)
-        
-    
+    pagination = Pagination(total=6, per_page=PAGE_COUNT, page=page_number)
+    return render_template('index.html', articles=article, pagination=pagination)
+
+
 @app.route('/add', methods=['GET', 'POST'])
 def add_article():
     if request.method == 'POST':
@@ -82,12 +91,13 @@ def add_article():
             # convert html
             html = markdown.markdown(md)
             # generate filename
-            filename = slug+'.md'
+            filename = slug + '.md'
             # save to SQLite
-            g.db.execute('insert into article (title, slug, file, html) values (?, ?, ?, ?)', [title, slug, filename, html])
+            g.db.execute('insert into article (title, slug, file, html) values (?, ?, ?, ?)',
+                         [title, slug, filename, html])
             g.db.commit()
             # save .md doc
-            file = codecs.open('md/'+filename, mode='w', encoding="utf-8")
+            file = codecs.open('md/' + filename, mode='w', encoding="utf-8")
             file.write(md)
             file.close()
             flash('New entry was successfully posted')
@@ -97,18 +107,18 @@ def add_article():
             return redirect(url_for('login'))
         else:
             return render_template('add.html')
-    
+
 
 @app.route('/read/<slug>')
 def read(slug):
     cur = g.db.execute('select title, time, html from article where slug = ?', [slug])
     article = [dict(title=row[0], time=row[1], html=row[2], slug=slug) for row in cur.fetchall()]
     return render_template('article.html', articles=article)
-    
-    
-@app.route('/md/<filename>') 
-def download_file(filename): 
-    return send_from_directory(app.config['MD_FOLDER'],filename, as_attachment=True)
+
+
+@app.route('/md/<filename>')
+def download_file(filename):
+    return send_from_directory(app.config['MD_FOLDER'], filename, as_attachment=True)
 
 
 if __name__ == '__main__':
