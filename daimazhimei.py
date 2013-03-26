e58d522255f1cd7cf98c3081aaa44eaa730c8283
xiaozhui <yuhanjian@yahoo.com>
2013-03-26 23:26:43 +0800

    rewrite front-end

--- daimazhimei.py
+++ daimazhimei.py
@@ -58,17 +58,20 @@ def teardown_request(exception):
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != app.config['USERNAME']:
-            error = 'Invalid username'
-        elif request.form['password'] != app.config['PASSWORD']:
-            error = 'Invalid password'
-        else:
-            session['logged_in'] = True
-            flash('You were logged in')
-            return redirect(url_for('index'))
-    return render_template('login.html', error=error)
+    if session.get('logged_in'):
+        return redirect(url_for('index'))
+    else:
+        error = None
+        if request.method == 'POST':
+            if request.form['username'] != app.config['USERNAME']:
+                error = 'Invalid username'
+            elif request.form['password'] != app.config['PASSWORD']:
+                error = 'Invalid password'
+            else:
+                session['logged_in'] = True
+                flash('You were logged in')
+                return redirect(url_for('index'))
+        return render_template('login.html', error=error)
 
 
 @app.route('/logout')
--- static/js/common.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-	$('.close-parent').click(function(e) {
-		$(this).parent().remove();
-	})
-});
